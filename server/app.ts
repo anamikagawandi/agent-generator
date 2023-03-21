9623f4234d284b7ce21ab7fe219ffcababcb288d
Anamika Gawandi <anamika.gawandi@gmail.com>
2023-03-21 01:16:03 -0400

    base logic for graphql

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,8 @@ import winston from "winston";
 import expressWinston from "express-winston";
 import { errorHandler } from "./helper/error-handler";
 import helmet from "helmet";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 /**
  * Initializes the express application
@@ -30,9 +32,25 @@ module.exports = async () => {
     res.status(200).json({ message: "ok" });
   });
 
-  app.use("/", (_req: Request, res: Response) => {
-    res.status(200).json({ message: "ok" });
-  });
+  // Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+type Query {
+  hello: String!
+}
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+hello: () => {
+  return 'Hello world!';
+},
+};
+
+  app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
 
   app.use(errorHandler);
   return app;
