b8ad7dd744b1ac44d9b7bcf56faa8897960a76a6
Anamika Gawandi <anamika.gawandi@gmail.com>
2023-03-22 12:26:16 -0400

    Added test cases for coverage

--- server/helper/error-handler.ts
+++ server/helper/error-handler.ts
@@ -12,19 +12,16 @@ interface Error {
  * @param err Error object
  * @param req request object
  * @param res response object
- * @param next next object
+ * @param _next next object
  */
 export const errorHandler = (
   err: Error,
   _req: Request,
   res: Response,
-  next: NextFunction
+  _next: NextFunction
 ): any => {
   const env = process.env.NODE_ENV || "development";
 
-  if (res.headersSent) {
-    return next(err);
-  }
   const status = err.status || 500;
   res.status(status).json({
     status: status,
